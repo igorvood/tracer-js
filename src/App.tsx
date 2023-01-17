1276110d62e2e1425a9082bb44f35428493b962d
vood <balitskY_igor@list.ru>
2023-01-17 16:30:34 +0300

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {TracerPage} from "./pages/TracerPage";
+import {HomePage} from "./pages/HomePage";
+import {Navigation} from "./components/Navigation";
 
 function App() {
-  return (
-      <h1 className="font-bold">asdfasdfasdf</h1>
-  );
+    return (
+        <>
+            <Navigation/>
+            <h1>asdfasdfsdf</h1>
+            <Routes>
+                <Route path="/" element={<TracerPage/>}></Route>
+                <Route path="/homePage" element={<HomePage/>}></Route>
+
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/TracerPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function TracerPage (){
+    return(
+        <h1>TracerPage</h1>
+    )
+}
