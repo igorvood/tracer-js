85e6a4e1063ad6f6270b956d992de5677986ce53
vood <balitskY_igor@list.ru>
2023-01-17 18:07:39 +0300

    sd

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,7 +8,7 @@ export function Navigation() {
         <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
              <h3 className="font-bold">Kafka Tracer</h3>
         <span>
-            <Link to="/" className="mr-2">Tracer</Link>
+            {/*<Link to="/" className="mr-2">Tracer</Link>*/}
         </span>
         {/*//     <span>*/}
         {/*//     <Link to="/" className="mr-2">Tracer</Link>*/}
