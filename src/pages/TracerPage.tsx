d0fe4a3c45470848db6099bca21bc80d45399fcf
vood <balitskY_igor@list.ru>
2023-01-18 14:30:22 +0300

    sd

--- src/pages/TracerPage.tsx
+++ src/pages/TracerPage.tsx
@@ -1,25 +1,34 @@
 import React, {useEffect, useState} from "react";
-import {useGroupListLikeQuery} from "../store/tracer/tracer.api";
+import {useGetGraphByGroupQuery, useGroupListLikeQuery} from "../store/tracer/tracer.api";
 import {useDebounce} from "../hooks/debounce";
 
 export function TracerPage() {
 
     const [search, setSearch] = useState('')
-    const debouncedSearch = useDebounce(search)
-    const {isLoading, isError, data: groups} = useGroupListLikeQuery(debouncedSearch, {
-        skip: debouncedSearch.length < 2
+    let [dropDown, setDropDown] = useState(false);
+    const debouncedSearchUser = useDebounce(search)
+    const {isLoading: isLoadingUser, isError: isErrorUser, data: groups} = useGroupListLikeQuery(debouncedSearchUser, {
+        skip: debouncedSearchUser.length < 2
     })
 
 
+    const {isLoading: isLoadingGraph, isError: isErrorGraph, data: graph} = useGetGraphByGroupQuery(debouncedSearchUser)
+
+
     useEffect(() =>{
-        console.log(debouncedSearch)
+        // показывать дроп даун если
+        setDropDown(debouncedSearchUser.length > 2 && groups?.length! > 0)
         },
-        [debouncedSearch]
+        [debouncedSearchUser, groups]
     )
+
+    const clickDropDownHandler = (groupId: String) =>  {
+
+    }
 
     return (
         <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
-            { isError && <p className="text-center text-red-600">Не удалось получить граф</p> }
+            { isErrorUser && <p className="text-center text-red-600">Не удалось получить граф</p> }
             <div className="relative w-[560px]">
                 <input
                     type="text"
@@ -28,9 +37,16 @@ export function TracerPage() {
                     value={search}
                     onChange={e => setSearch(e.target.value)}
                 />
-                <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
-                    { isLoading && <p className="text-center">Loading...</p> }
-                </ul>
+                {dropDown && <ul
+                    className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+                    {isLoadingUser && <p className="text-center">Loading...</p>}
+                    {groups?.map(gr => <li key={gr.id}
+                                           className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+                                           onClick={() => clickDropDownHandler(gr.id)}
+                        >{gr.id}</li>
+                    )
+                    }
+                </ul>}
 
             </div>
         </div>
