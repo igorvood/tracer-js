275efdcbc3a7f2b7b8863543a3b727f08629ffbf
vood <balitskY_igor@list.ru>
2023-01-18 09:27:41 +0300

    sd

--- src/pages/TracerPage.tsx
+++ src/pages/TracerPage.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useGetGraphByGroupQuery} from "../store/tracer/tracer.api";
+import {useDebounce} from "../hooks/debounce";
 
 export function TracerPage() {
 
-    const {isLoading, isError, data} = useGetGraphByGroupQuery('rto_graph')
+    const [search, setSearch] = useState('')
+    const debouncedSearch = useDebounce(search)
+    const {isLoading, isError, data} = useGetGraphByGroupQuery(debouncedSearch)
 
-    console.log(data)
+
+    useEffect(() =>{
+        console.log(debouncedSearch)
+        },
+        [debouncedSearch]
+    )
+
     return (
         <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
             { isError && <p className="text-center text-red-600">Не удалось получить граф</p> }
@@ -14,8 +23,8 @@ export function TracerPage() {
                     type="text"
                     className="border py-2 px-4 w-full h-[42px] mb-2"
                     placeholder="Search group for graph..."
-                    // value={search}
-                    // onChange={e => setSearch(e.target.value)}
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
                 />
                 <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
                     тут список групп графов
