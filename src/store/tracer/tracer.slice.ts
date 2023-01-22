4d6deb88fbd6cb110615bb87e77017c5173ef3e2
vood <balitskY_igor@list.ru>
2023-01-22 17:45:32 +0300

    sd

---
+++ src/store/tracer/tracer.slice.ts
@@ -0,0 +1,26 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit'
+
+const LS_ID_GROUP = 'LS_ID_GROUP'
+
+interface TracerState {
+  idGroup: string
+}
+
+const initialState: TracerState = {
+  idGroup: JSON.parse(localStorage.getItem(LS_ID_GROUP) ?? '')
+}
+
+export const tracerSlice = createSlice({
+  name: 'tracerState',
+  initialState,
+  reducers: {
+    rememberIdGroup(state, action: PayloadAction<string>) {
+      state.idGroup = action.payload
+      localStorage.setItem(LS_ID_GROUP, JSON.stringify(state.idGroup))
+    },
+
+  }
+})
+
+export const tracerActions = tracerSlice.actions
+export const tracerReducer = tracerSlice.reducer
